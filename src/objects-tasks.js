a13e914502e8c71ba4a6034fc562d3acbe249312
May <abeillee@bk.ru>
2024-12-25 05:33:47 +0300

    featr: add solution for compareObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -69,8 +69,12 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  let result;
+  if (obj1.length === obj2.length) {
+    result = Object.keys(obj1).every((key) => obj1[key] === obj2[key]);
+  }
+  return result;
 }
 
 /**

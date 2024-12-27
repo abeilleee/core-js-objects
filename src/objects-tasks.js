c2c4b037a9f8ce545b55a7c1ce6f4a22c8453d12
May <abeillee@bk.ru>
2024-12-27 10:43:53 +0300

    feat: add solution for makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -129,8 +129,16 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const maxIdx = Math.max(...Object.values(lettersObject).flat());   //делаю одномерный массив(flat) и нахожу максимум
+  const result = Array.from({ length: maxIdx }).fill('');
+
+  Object.entries(lettersObject).forEach(([key, value]) => {
+    value.forEach((idx) => {
+      result[idx] = key;
+    });
+  });
+  return result.join('');
 }
 
 /**
